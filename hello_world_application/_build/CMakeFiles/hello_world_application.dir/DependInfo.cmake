af28896db1b9c7f191e55b6bc04c3db5f233d493
vasinaa <99878647+vasinaa@users.noreply.github.com>
2022-05-21 14:24:54 +0300

    Add files via upload

---
+++ hello_world_application/_build/CMakeFiles/hello_world_application.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexandra/lab03/hello_world_application/hello_world.cpp" "/home/alexandra/lab03/hello_world_application/_build/CMakeFiles/hello_world_application.dir/hello_world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../formatter_lib"
+  "../../formatter_ex_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alexandra/lab03/hello_world_application/_build/CMakeFiles/formatter_ex_lib.dir/DependInfo.cmake"
+  "/home/alexandra/lab03/hello_world_application/_build/CMakeFiles/formatter_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hello_world_application/_build/CMakeFiles/hello_world_application.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_application.dir/hello_world.cpp.o"
+  "hello_world_application"
+  "hello_world_application.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world_application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
